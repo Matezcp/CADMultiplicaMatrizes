06458a22f5922cb2e02e0373c2ab9c68201c2d6b
Matezcp <matezcp@hotmail.com>
2021-11-21 20:10:39 -0300

    efficiently parallelized

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*
+GRUPO:
+Mateus Zanetti Camargo Penteado - 11219202
+Yure Pablo do Nascimento Oliveira - 11275317
+Breno Alves de Sousa - 11345555
+*/
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
@@ -5,20 +11,19 @@
 #include <omp.h>
 #include"mpi.h"
 
-#define TAG 13
 //Matrix N X N
-#define N 3
+#define N 10
 //Número de Threads para paralelização
-#define NUMTHREADS 2
+#define NUMTHREADS 8
 //Para debug
-#define DEBUG
+//#define DEBUG
 
 int main(int argc, char **argv){
-    int *A,*Aoriginal,*B,*C,*Cfinal;
+    long long int *A,*Aoriginal,*B,*C,*Cfinal;
     int numProcs,myRank,cargaTrabalho;
-    int numThreads = NUMTHREADS;
-    int i,j,k;
-    double startTime, endTime;
+    long long int numThreads = NUMTHREADS;
+    long long int i,j,k;
+    double wtime;
     MPI_Comm USED_PROCESS;
 
     //Inicializa o MPI e pega o número de processos e o rank do processo atual
@@ -41,30 +46,30 @@ int main(int argc, char **argv){
     //Coloca os processos que serão usados em um sub-comunicador com cor 1 e os processo que não irão ser utilizados em outro com cor 0
     MPI_Comm_split(MPI_COMM_WORLD, (myRank<N), myRank, &USED_PROCESS);
 
-    //Processos alocam as matrizes que irão utilizar nos calculos
+    //Processos alocam as matrizes que irão utilizar nos calculos (as matrizes são alocadas como vetores, pois precisamos que os dados
+    //fiquem sequenciais na memória)
     if(myRank < N){
         //Aloca a matriz A
-        A = (int *) malloc(sizeof(int) * N * cargaTrabalho);
+        A = (long long int *) malloc(sizeof(long long int) * N * cargaTrabalho);
         //Aloca a matriz B
-        B = (int *) malloc(sizeof(int) * N * N);
+        B = (long long int *) malloc(sizeof(long long int) * N * N);
         //Aloca a matriz C
-        C = (int *) calloc(N * cargaTrabalho,sizeof(int));
+        C = (long long int *) calloc(N * cargaTrabalho,sizeof(long long int));
     }
 
     //Processo 0 aloca as matrizes originais (a matriz B já foi alocada)
     if(myRank == 0){
         //Aloca a matriz A original
-        Aoriginal = (int *) malloc (sizeof(int) * N * N);
+        Aoriginal = (long long int *) malloc (sizeof(long long int) * N * N);
         //Aloca a matriz C Final
-        Cfinal = (int *) calloc (N*N,sizeof(int));
+        Cfinal = (long long int *) calloc (N*N,sizeof(long long int));
 
-        //Preenche Aoriginal e B (NÃO TÁ ALEÁTORIO LÁ DO JEITO QUE TEM SER)
+        //Preenche Aoriginal e B
         for(i = 0; i < N; i++){
             for(j = 0; j < N; j++){
-                //Aoriginal[i][j] = k;
-                //B[i][j] = k;
-                Aoriginal[i*N+j] = rand();
-                B[i*N+j] = rand();
+                //Limitamos a 50 os valores das matrizes
+                Aoriginal[i*N+j] = rand() % 50;
+                B[i*N+j] = rand() % 50;
             }
         }
 
@@ -72,7 +77,7 @@ int main(int argc, char **argv){
         printf("Aoriginal: \n");
         for(i = 0; i < N; i++){
             for(j = 0; j < N; j++){
-                printf("%d ",Aoriginal[i*N+j]);
+                printf("%lld ",Aoriginal[i*N+j]);
             }
             printf("\n");
         }
@@ -80,7 +85,7 @@ int main(int argc, char **argv){
         printf("B: \n");
         for(i = 0; i < N; i++){
             for(j = 0; j < N; j++){
-                printf("%d ",B[i*N+j]);
+                printf("%lld ",B[i*N+j]);
             }
             printf("\n");
         }
@@ -92,35 +97,34 @@ int main(int argc, char **argv){
     
     if(myRank < N){
         //Manda um pedaço da matriz Aoriginal equivalente a carga de trabalho para cada processo
-        if(MPI_Scatter(Aoriginal, N*cargaTrabalho, MPI_INT, A, N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
+        if(MPI_Scatter(Aoriginal, N*cargaTrabalho, MPI_LONG_LONG_INT, A, N*cargaTrabalho, MPI_LONG_LONG_INT, 0, USED_PROCESS) != MPI_SUCCESS){
             printf("SCATTER ERROR\n");
             exit(1);
         }
         //Manda a matrix B inteira para todos processos
-        MPI_Bcast(B,N*N,MPI_INT,0,USED_PROCESS);
+        MPI_Bcast(B,N*N,MPI_LONG_LONG_INT,0,USED_PROCESS);
 
         //Começa o timer
         if (myRank == 0) {
-            startTime = omp_get_wtime();
+            wtime = omp_get_wtime();
         }
 
-        //Cada processo faz seus calculos paralelamente
+        //Cada processo faz seus calculos paralelamente, primeiro colapsamos os 2 primeiros for's
         #pragma omp parallel for collapse(2)
         for (i = 0; i < cargaTrabalho; i++) {
             for (j = 0; j < N; j++) {
-                int aux = 0;
+                long long int soma = 0;
 
                 #ifdef DEBUG
-                    printf("On thread %d, i = %d, j = %d\n", omp_get_thread_num(), i, j);
+                    printf("On thread %d, i = %lld, j = %lld\n", omp_get_thread_num(), i, j);
                 #endif
-
-                #pragma omp parallel for simd reduction(+:aux)
+                //Em seguida paralelizamos o for mais interno com simd e redução da variável soma
+                #pragma omp parallel for simd reduction(+:soma)
                 for (k = 0; k < N; k++) {
-                    //C[i][j] += A[i][k] * B[k][j];
-                    aux += A[i*N+k] * B[k*N+j];
+                    soma += A[i*N+k] * B[k*N+j];
                 }
-
-                C[i*N+j] = aux;
+                //Atualizamos o valor de C calculado
+                C[i*N+j] = soma;
             }
         }
 
@@ -128,63 +132,56 @@ int main(int argc, char **argv){
             //Verifica se a carga de trabalho não foi exata
             if(numProcs*cargaTrabalho != N){
                 //Ve o quanto falta computar
-                int faltantes = N - numProcs*cargaTrabalho;
-                //Computa o que falta
-
+                long long int faltantes = N - numProcs*cargaTrabalho;
+                //Computa o que falta, primeiro colapsamos os 2 primeiros for's
                 #pragma omp parallel for collapse(2)
                 for (i = N-1; i >= N-faltantes; i--) {
                     for (j = 0; j < N; j++) {
-                        int aux = 0;
-
-                        #pragma omp parallel for simd reduction(+:aux)
+                        long long int soma = 0;
+                        //Em seguida paralelizamos o for mais interno com simd e redução da variável soma
+                        #pragma omp parallel for simd reduction(+:soma)
                         for (k = 0; k < N; k++) {
-                            aux += Aoriginal[i*N+k] * B[k*N+j];
+                            soma += Aoriginal[i*N+k] * B[k*N+j];
                         }
-
-                        Cfinal[i*N+j] = aux;
+                        //Atualizamos o valor de C calculado
+                        Cfinal[i*N+j] = soma;
                     }
                 }
             }
         }
 
         //O processo 0 recebe as partes de cada processo
-        if(MPI_Gather(C, N*cargaTrabalho, MPI_INT, Cfinal, N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
+        if(MPI_Gather(C, N*cargaTrabalho, MPI_LONG_LONG_INT, Cfinal, N*cargaTrabalho, MPI_LONG_LONG_INT, 0, USED_PROCESS) != MPI_SUCCESS){
             printf("GATHER ERROR\n");
             exit(1);
         }
 
         //Termina o timer
         if(myRank == 0){
-            endTime = omp_get_wtime();
+            wtime = omp_get_wtime() - wtime;
         }
 
         //Printa o resultado Final
         if(myRank == 0){
             for(i = 0; i < N; i++){
                 for(j = 0; j < N; j++){
-                    //printf("%d ",Cfinal[i][j]);
-                    printf("%d ",Cfinal[i*N+j]);
+                   printf("%lld ",Cfinal[i*N+j]);
                 }
                 printf("\n");
             }
-
-            printf("Tempo passado: %lf\n", endTime - startTime);
+            printf("Tempo passado: %lf\n", wtime);
         }
     }
 
     //Espera todos processos chegarem aqui antes de finalizar as execuções
     MPI_Barrier(MPI_COMM_WORLD);
+    //Libera o que foi alocado
     if(myRank < N){
-        //free(A[0]);
         free(A);
-        //free(B[0]);
         free(B);
-        //free(C[0]);
         free(C);
         if(myRank == 0){
-            //free(Aoriginal[0]);
             free(Aoriginal);
-            //free(Cfinal[0]);
             free(Cfinal);
         }
     }
