665a2feccd9a1691147c2d2d958145f0e3a7bf7e
Matezcp <matezcp@hotmail.com>
2021-11-21 18:07:13 -0300

    working with parallel

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define NUMTHREADS 8
 
 int main(int argc, char **argv){
-    int **A,**Aoriginal,**B,**C,**Cfinal,*tmp;
+    int *A,*Aoriginal,*B,*C,*Cfinal;
     int numProcs,myRank,cargaTrabalho;
     int numThreads = NUMTHREADS;
     int i,j,k;
@@ -32,51 +32,34 @@ int main(int argc, char **argv){
     else
         cargaTrabalho = N/numProcs;
 
-    //Coloca os processos que serão usados em um sub-comunicador e os processo que não irão ser utilizados em outro
+    //Coloca os processos que serão usados em um sub-comunicador com cor 1 e os processo que não irão ser utilizados em outro com cor 0
     MPI_Comm_split(MPI_COMM_WORLD, (myRank<N), myRank, &USED_PROCESS);
 
     //Processos alocam as matrizes que irão utilizar nos calculos
     if(myRank < N){
         //Aloca a matriz A
-        tmp = (int *) malloc (sizeof(int) * N * cargaTrabalho);
-        A = (int **) malloc(sizeof(int*) * cargaTrabalho);
-        for(i = 0; i < cargaTrabalho; i++){
-            A[i] = &tmp[i * N];
-        }
+        A = (int *) malloc(sizeof(int) * N * cargaTrabalho);
         //Aloca a matriz B
-        tmp = (int *) malloc (sizeof(int) * N * N);
-        B = (int **) malloc(sizeof(int*) * N);
-        for(i = 0; i < N; i++){
-            B[i] = &tmp[i * N];
-        }
+        B = (int *) malloc(sizeof(int) * N * N);
         //Aloca a matriz C
-        tmp = (int *) malloc (sizeof(int) * N * cargaTrabalho);
-        C = (int **) malloc(sizeof(int*) * cargaTrabalho);
-        for(i = 0; i < cargaTrabalho; i++){
-            C[i] = &tmp[i * N];
-        }
+        C = (int *) malloc (sizeof(int) * N * cargaTrabalho);
     }
 
     //Processo 0 aloca as matrizes originais (a matriz B já foi alocada)
     if(myRank == 0){
         //Aloca a matriz A original
-        tmp = (int *) malloc (sizeof(int) * N * N);
-        Aoriginal = (int **) malloc(sizeof(int*) * N);
-        for(i = 0; i < N; i++){
-            Aoriginal[i] = &tmp[i * N];
-        }
+        Aoriginal = (int *) malloc (sizeof(int) * N * N);
         //Aloca a matriz C Final
-        tmp = (int *) malloc (sizeof(int) * N * N);
-        Cfinal = (int **) malloc(sizeof(int*) * N);
-        for(i = 0; i < N; i++){
-            Cfinal[i] = &tmp[i * N];
-        }
+        Cfinal = (int *) malloc (sizeof(int) * N * N);
+
         //Preenche Aoriginal e B (NÃO TÁ ALEÁTORIO LÁ DO JEITO QUE TEM SER)
         k = 0;
         for(i = 0; i < N; i++){
             for(j = 0; j < N; j++){
-                Aoriginal[i][j] = k;
-                B[i][j] = k;
+                //Aoriginal[i][j] = k;
+                //B[i][j] = k;
+                Aoriginal[i*N+j] = k;
+                B[i*N+j] = k;
                 k++;
             }
         }
@@ -85,32 +68,34 @@ int main(int argc, char **argv){
     
     if(myRank < N){
         //Manda um pedaço da matriz Aoriginal equivalente a carga de trabalho para cada processo
-        if(MPI_Scatter(Aoriginal[0], N*cargaTrabalho, MPI_INT, A[0], N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
+        if(MPI_Scatter(Aoriginal, N*cargaTrabalho, MPI_INT, A, N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
             printf("SCATTER ERROR\n");
             exit(1);
         }
         //Manda a matrix B inteira para todos processos
-        MPI_Bcast(B[0],N*N,MPI_INT,0,USED_PROCESS);
+        MPI_Bcast(B,N*N,MPI_INT,0,USED_PROCESS);
 
         //Inicializa a matriz C
         for(i = 0; i < cargaTrabalho; i++){
             for(j = 0; j < N; j++){
-                C[i][j] = 0;
+                //C[i][j] = 0;
+                C[i*N+j] = 0;
             }
         }
 
         //Cada processo faz seus calculos paralelamente
-        //#pragma omp parallel for shared(A,B,C,numThreads) private(i,j,k) schedule (static, 10)
+        #pragma omp parallel for shared(A,B,C) private(i,j,k) schedule (static)
         for (i = 0; i < cargaTrabalho; i++) {
             for (j = 0; j < N; j++) {
                 for (k = 0; k < N; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
+                    //C[i][j] += A[i][k] * B[k][j];
+                    C[i*N+j] += A[i*N+k] * B[k*N+j];
                 }
             }
         }
 
         //O processo 0 recebe as partes de cada processo
-        if(MPI_Gather(C[0], N*cargaTrabalho, MPI_INT, Cfinal[0], N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
+        if(MPI_Gather(C, N*cargaTrabalho, MPI_INT, Cfinal, N*cargaTrabalho, MPI_INT, 0, USED_PROCESS) != MPI_SUCCESS){
             printf("GATHER ERROR\n");
             exit(1);
         }
@@ -119,7 +104,8 @@ int main(int argc, char **argv){
         if(myRank == 0){
             for(i = 0; i < N; i++){
                 for(j = 0; j < N; j++){
-                    printf("%d ",Cfinal[i][j]);
+                    //printf("%d ",Cfinal[i][j]);
+                    printf("%d ",Cfinal[i*N+j]);
                 }
                 printf("\n");
             }
@@ -128,6 +114,20 @@ int main(int argc, char **argv){
 
     //Espera todos processos chegarem aqui antes de finalizar as execuções
     MPI_Barrier(MPI_COMM_WORLD);
+    if(myRank < N){
+        //free(A[0]);
+        free(A);
+        //free(B[0]);
+        free(B);
+        //free(C[0]);
+        free(C);
+        if(myRank == 0){
+            //free(Aoriginal[0]);
+            free(Aoriginal);
+            //free(Cfinal[0]);
+            free(Cfinal);
+        }
+    }
     MPI_Finalize();
     return 0;
 }
